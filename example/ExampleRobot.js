ce83f2147b3f119828fde1ac36403657d6e06ad8
Bo HOU <bhoustudio@gmail.com>
2016-05-09 00:37:06 +0200

    add example folder

---
+++ example/ExampleRobot.js
@@ -0,0 +1,56 @@
+const i11e = require('../lib/dep').i11e;
+
+module.exports = i11e.createRobot({
+  initRobot() {
+  },
+
+  input() {
+    return {
+      "name:?&": "John"  // name, optional, if exists, type must be string
+    }
+  },
+
+  output() {
+    return {
+      "greetings": (box, v) => {
+        return v === `Hello World, ${box.get('name')}`;
+      }
+    }
+  },
+
+  filter(box) {
+    return true;
+  },
+
+  process(box, done) {
+    var name = box.get('name') || 'Guest';
+
+    box.set('greetings', `Hello World, ${name}`);
+
+    done(null, box);
+  },
+
+  examples() {
+    return {
+      "basic usage": {
+        options: { // options
+        },
+        input: { // input
+          "name": "John"
+        },
+        output: { // output
+          "name": "John",
+          "greetings": "Hello World, John"
+        }
+      },
+
+      "greeting without name": {
+        options: {},
+        input: {},
+        output: {
+          "greetings": "Hello World, Guest"
+        }
+      }
+    };
+  }
+});
